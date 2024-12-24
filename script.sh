0051123c2ac182bc4dfb854b96a6b95d4243cf26
satishyekula548 <satishyekula548@gmail.com>
2024-12-24 11:01:27 +0530

    Initial commit

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Initialize a new Git repository
+git init
+
+# Add all files to the staging area
+git add .
+
+# Commit the changes
+git commit -m "Initial commit"
+
+# Add the GitLab remote repository
+git remote add origin https://github.com/satishyekula548/zelar-pratices.git
+
+# Push the changes to GitLab
+git push -u origin master
+
